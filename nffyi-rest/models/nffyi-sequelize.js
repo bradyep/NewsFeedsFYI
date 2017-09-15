02a7e1f25a999d6cec53dc63ff3888c5c8ff2a24
Brady Prigge <syntonicstudios@gmail.com>
2017-09-15 14:22:43 -0500

    Added first bootstrap-react Modal

--- nffyi-rest/models/nffyi-sequelize.js
+++ nffyi-rest/models/nffyi-sequelize.js
@@ -256,6 +256,19 @@ function connectDB(modelRequested) {
         .then(function ([instance, created]) {
         log(instance.get({ plain: true }));
         log(created);
+        log('--Creating Initial Data: Page(Pro Page One)--');
+        return models.SQPage.findOrCreate({
+            where: {
+                pageID: 2, Name: 'Pro Page One'
+            },
+            defaults: {
+                userID: 3, name: 'Pro Page One', displayOrder: 1
+            }
+        });
+    })
+        .then(function ([instance, created]) {
+        log(instance.get({ plain: true }));
+        log(created);
         log('--Creating Initial Data: FeedSource(BBC World News)--');
         return models.SQFeedSource.findOrCreate({
             where: {
@@ -295,6 +308,175 @@ function connectDB(modelRequested) {
         .then(function ([instance, created]) {
         log(instance.get({ plain: true }));
         log(created);
+        log('--Creating Initial Data: FeedSource(Hacker News)--');
+        return models.SQFeedSource.findOrCreate({
+            where: {
+                url: 'https://news.ycombinator.com/rss', feedSourceID: 4
+            },
+            defaults: {
+                cachedTitle: 'Hacker News', url: 'https://news.ycombinator.com/rss', cachedWebsiteURL: 'https://news.ycombinator.com/', lastCachedDate: Date()
+            }
+        });
+    })
+        .then(function ([instance, created]) {
+        log(instance.get({ plain: true }));
+        log(created);
+        log('--Creating Initial Data: CachedNewsItem(BBC 1)--');
+        return models.SQCachedNewsItem.findOrCreate({
+            where: {
+                cachedNewsItemID: 1, feedSourceID: 1
+            },
+            defaults: {
+                title: "Donald Trump: N Korea's Kim Jong-un a 'smart cookie'", link: 'http://www.bbc.co.uk/news/world-asia-39764834', description: "The US president reflects on the young leader's rise and says 'we'll see' about US military options.", feedSourceID: 1
+            }
+        });
+    })
+        .then(function ([instance, created]) {
+        log(instance.get({ plain: true }));
+        log(created);
+        log('--Creating Initial Data: CachedNewsItem(BBC 2)--');
+        return models.SQCachedNewsItem.findOrCreate({
+            where: {
+                cachedNewsItemID: 2, feedSourceID: 1
+            },
+            defaults: {
+                title: "Dozens of Yazidis enslaved by IS in Iraq now free", link: 'http://www.bbc.co.uk/news/world-middle-east-39762790', description: "The 36 men, women and children were held by IS in Iraq for nearly three years.", feedSourceID: 1
+            }
+        });
+    })
+        .then(function ([instance, created]) {
+        log(instance.get({ plain: true }));
+        log(created);
+        log('--Creating Initial Data: CachedNewsItem(BBC 3)--');
+        return models.SQCachedNewsItem.findOrCreate({
+            where: {
+                cachedNewsItemID: 3, feedSourceID: 1
+            },
+            defaults: {
+                title: "Ueli Steck: Everest preparation claims 'Swiss Machine' climber", link: 'http://www.bbc.co.uk/news/world-asia-39761904', description: "'Swiss Machine' Ueli Steck dies in an accident while acclimatising for an attempt on a new route.", feedSourceID: 1
+            }
+        });
+    })
+        .then(function ([instance, created]) {
+        log(instance.get({ plain: true }));
+        log(created);
+        log('--Creating Initial Data: CachedNewsItem(NYTimes 1)--');
+        return models.SQCachedNewsItem.findOrCreate({
+            where: {
+                cachedNewsItemID: 4, feedSourceID: 2
+            },
+            defaults: {
+                title: "Trump’s ‘Very Friendly’ Talk With Duterte Stuns Aides and Critics Alike", link: 'https://www.nytimes.com/2017/04/30/us/politics/trump-duterte.html?partner=rss&emc=rss', description: "The administration is bracing for an avalanche of criticism after the president embraced Rodrigo Duterte, who has led a deadly crackdown on drugs in the Philippines.", feedSourceID: 2
+            }
+        });
+    })
+        .then(function ([instance, created]) {
+        log(instance.get({ plain: true }));
+        log(created);
+        log('--Creating Initial Data: CachedNewsItem(NYTimes 2)--');
+        return models.SQCachedNewsItem.findOrCreate({
+            where: {
+                cachedNewsItemID: 5, feedSourceID: 2
+            },
+            defaults: {
+                title: "Becoming Duterte: The Making of a Philippine Strongman", link: 'http://www.nytimes.com/2017/03/21/world/asia/rodrigo-duterte-philippines-president-strongman.html?partner=rss&emc=rss', description: "He is a child of privilege turned populist politician, an antidrug crusader who has struggled with drug abuse. Obsessed with death, he has turned his violent vision into national policy.", feedSourceID: 2
+            }
+        });
+    })
+        .then(function ([instance, created]) {
+        log(instance.get({ plain: true }));
+        log(created);
+        log('--Creating Initial Data: CachedNewsItem(NYTimes 3)--');
+        return models.SQCachedNewsItem.findOrCreate({
+            where: {
+                cachedNewsItemID: 6, feedSourceID: 2
+            },
+            defaults: {
+                title: "News Analysis: On Trade, a Politically Feisty Trump Risks Economic Damage", link: 'https://www.nytimes.com/2017/04/30/business/trump-nafta-trade-economy.html?partner=rss&emc=rss', description: "Trucks waiting to enter the United States at the border crossing in Tijuana, Mexico, in February.", feedSourceID: 2
+            }
+        });
+    })
+        .then(function ([instance, created]) {
+        log(instance.get({ plain: true }));
+        log(created);
+        log('--Creating Initial Data: CachedNewsItem(ESPN 1)--');
+        return models.SQCachedNewsItem.findOrCreate({
+            where: {
+                cachedNewsItemID: 7, feedSourceID: 3
+            },
+            defaults: {
+                title: "Jazz hold off Clippers to win series", link: 'http://www.espn.com/nba/recap?gameId=400950426', description: "Jazz hold off Clippers to win series", feedSourceID: 3
+            }
+        });
+    })
+        .then(function ([instance, created]) {
+        log(instance.get({ plain: true }));
+        log(created);
+        log('--Creating Initial Data: CachedNewsItem(ESPN 2)--');
+        return models.SQCachedNewsItem.findOrCreate({
+            where: {
+                cachedNewsItemID: 8, feedSourceID: 3
+            },
+            defaults: {
+                title: "Nats' Rendon goes 6-for-6, with 3 HRs, 10 RBIs", link: 'http://www.espn.com/mlb/story/_/id/19281935/anthony-rendon-washington-nationals-goes-6-6-3-hrs-10-rbis', description: "Nats' Rendon goes 6-for-6, with 3 HRs, 10 RBIs", feedSourceID: 3
+            }
+        });
+    })
+        .then(function ([instance, created]) {
+        log(instance.get({ plain: true }));
+        log(created);
+        log('--Creating Initial Data: CachedNewsItem(ESPN 3)--');
+        return models.SQCachedNewsItem.findOrCreate({
+            where: {
+                cachedNewsItemID: 9, feedSourceID: 3
+            },
+            defaults: {
+                title: "Toothless Thomas: C's star eyes fast dental fix", link: 'http://www.espn.com/nba/story/_/id/19282028/isaiah-thomas-boston-celtics-loses-tooth-game-1-win-washington-wizards', description: "Toothless Thomas: C's star eyes fast dental fix", feedSourceID: 3
+            }
+        });
+    })
+        .then(function ([instance, created]) {
+        log(instance.get({ plain: true }));
+        log(created);
+        log('--Creating Initial Data: CachedNewsItem(Hacker News 1)--');
+        return models.SQCachedNewsItem.findOrCreate({
+            where: {
+                cachedNewsItemID: 10, feedSourceID: 4
+            },
+            defaults: {
+                title: "Firefox Multi-Account Containers", link: 'https://blog.mozilla.org/firefox/introducing-firefox-multi-account-containers/', description: "Our new Multi-Account Containers extension for Firefox means you can finally wrangle multiple email/social accounts.", feedSourceID: 4
+            }
+        });
+    })
+        .then(function ([instance, created]) {
+        log(instance.get({ plain: true }));
+        log(created);
+        log('--Creating Initial Data: CachedNewsItem(Hacker News 2)--');
+        return models.SQCachedNewsItem.findOrCreate({
+            where: {
+                cachedNewsItemID: 11, feedSourceID: 4
+            },
+            defaults: {
+                title: "Zircon Kernel for Fuchsia OS", link: 'https://fuchsia.googlesource.com/zircon', description: "Zircon is the core platform that powers the Fuchsia OS. Zircon is composed of a microkernel (source in kernel/...) as well as a small set of userspace services, drivers, and libraries (source in system/...) necessary for the system to boot, talk to hardware, load userspace processes and run them, etc.", feedSourceID: 4
+            }
+        });
+    })
+        .then(function ([instance, created]) {
+        log(instance.get({ plain: true }));
+        log(created);
+        log('--Creating Initial Data: CachedNewsItem(Hacker News 3)--');
+        return models.SQCachedNewsItem.findOrCreate({
+            where: {
+                cachedNewsItemID: 12, feedSourceID: 4
+            },
+            defaults: {
+                title: "React issue #10719: Update React license FAQ/update license itself", link: 'https://github.com/facebook/react/issues/10719', description: "I've read the previous discussions regarding the React (and other Facebook OSS) license, in particular #10191, and I agree with most of the clarificatory remarks (esp. by @gaearon & @sebmarkbage) that Facebook doesn't intend to use this license offensively.", feedSourceID: 4
+            }
+        });
+    })
+        .then(function ([instance, created]) {
+        log(instance.get({ plain: true }));
+        log(created);
         log('--Creating Initial Data: UserFeed(BBC World News)--');
         return models.SQUserFeed.findOrCreate({
             where: {
@@ -328,6 +510,19 @@ function connectDB(modelRequested) {
             },
             defaults: {
                 pageID: 1, feedSourceID: 3, name: 'ESPN', column: 3, displayOrder: 1, itemDisplayCount: 3
+            }
+        });
+    })
+        .then(function ([instance, created]) {
+        log(instance.get({ plain: true }));
+        log(created);
+        log('--Creating Initial Data: UserFeed(Hacker News)--');
+        return models.SQUserFeed.findOrCreate({
+            where: {
+                pageID: 2, Name: 'Hacker News'
+            },
+            defaults: {
+                pageID: 2, feedSourceID: 4, name: 'Hacker News', column: 1, displayOrder: 1, itemDisplayCount: 3
             }
         });
     })
--- webapp/src/app/components/TopHeaderComponents/ProfileSection/index.tsx
+++ webapp/src/app/components/TopHeaderComponents/ProfileSection/index.tsx
@@ -2,13 +2,14 @@ import * as React from 'react';
 import * as styles from './styles.css';
 // import { TodoTextInput } from '../TodoTextInput';
 // import { TodoModel } from '../../models/TodoModel';
+import { Modal, Button } from "react-bootstrap";
 
 export interface ProfileSectionProps {
   // addTodo: (todo: Partial<TodoModel>) => any;
 }
 
 export interface ProfileSectionState {
-  /* empty */
+  showModal: boolean;
 }
 
 export class ProfileSection extends React.Component<ProfileSectionProps, ProfileSectionState> {
@@ -16,19 +17,63 @@ export class ProfileSection extends React.Component<ProfileSectionProps, Profile
   constructor(props?: ProfileSectionProps, context?: any) {
     super(props, context);
     // this.handleSave = this.handleSave.bind(this);
+    this.state = {
+      showModal: false
+    };
+    this.close = this.close.bind(this); 
+    this.open = this.open.bind(this); 
+    this.displayModal = this.displayModal.bind(this); 
+    this.renderSignInModal = this.renderSignInModal.bind(this); 
+    this.render = this.render.bind(this); 
   }
 
-/*   
-  handleSave(text: string) {
-    if (text.length) {
-      this.props.addTodo({ text });
+  /*   
+    handleSave(text: string) {
+      if (text.length) {
+        this.props.addTodo({ text });
+      }
     }
+     */
+
+  close() {
+    this.setState({ showModal: false });
   }
-   */
+
+  open() {
+    this.setState({ showModal: true });
+    console.log("Hey! " + this.state.showModal.toString());
+  }
+
+  displayModal(show: boolean): void {
+    this.setState({ showModal: show })
+  }
+
+  renderSignInModal() {
+    return (
+      <div className="static-modal" >
+        <Modal show={this.state.showModal} onHide={this.close}>
+          <Modal.Header>
+            <Modal.Title>Modal title</Modal.Title>
+          </Modal.Header>
+
+          <Modal.Body>
+            One fine body...
+        </Modal.Body>
+
+          <Modal.Footer>
+            <Button onClick={this.close}>Close</Button>
+            <Button bsStyle="primary">Save changes</Button>
+          </Modal.Footer>
+
+        </Modal>
+      </div>
+    )
+  }
 
   render() {
     return (
-      <div className={styles.profileSection}>
+      <div className={styles.profileSection} onClick={this.open}>
+        {this.renderSignInModal()}
         <i className="fa fa-user fa-2x" aria-hidden="true"></i>
         <p>
           Sign In To Customize
